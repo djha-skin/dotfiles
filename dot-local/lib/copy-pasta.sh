9c17d2536f8b5bc80f44735ca1bf220c8e0e43b5
Daniel Jay Haskin <daniel.haskin@angel.com>
2022-10-05 12:55:23 -0600

    I think we're there

---
+++ dot-local/lib/copy-pasta.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if which wl-copy >/dev/null 2>&1
+then
+    copy=wl-copy
+    paste=wl-paste
+elif which pbcopy >/dev/null 2>&1
+then
+    copy=pbcopy
+    paste=pbpaste
+else
+    copy=xsel -i -b
+    paste=xsel -o -b
+fi
