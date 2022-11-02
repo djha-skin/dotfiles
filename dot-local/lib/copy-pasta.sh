945176a6fc38cbbe1f99ef4f4e32898719c86cec
Daniel Jay Haskin <daniel.haskin@angel.com>
2022-11-02 07:44:16 -0600

    Tweaks for Wayland

--- dot-local/lib/copy-pasta.sh
+++ dot-local/lib/copy-pasta.sh
@@ -2,13 +2,13 @@
 
 if which wl-copy >/dev/null 2>&1
 then
-    copy=wl-copy
-    paste=wl-paste -n
+    copy="wl-copy"
+    paste="wl-paste -n"
 elif which pbcopy >/dev/null 2>&1
 then
-    copy=pbcopy
-    paste=pbpaste
+    copy="pbcopy"
+    paste="pbpaste"
 else
-    copy=xsel -i -b
-    paste=xsel -o -b
+    copy="xsel -i -b"
+    paste="xsel -o -b"
 fi
