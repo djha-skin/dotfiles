a9dd0884d3243fc012a055b973f522e396c1cb4b
Daniel Jay Haskin <me@djha.skin>
2023-01-04 10:32:03 -0700

    Screenshot region

--- dot-local/lib/copy-pasta.sh
+++ dot-local/lib/copy-pasta.sh
@@ -8,6 +8,9 @@ elif which pbcopy >/dev/null 2>&1
 then
     copy="pbcopy"
     paste="pbpaste"
+elif which copy >/dev/null 2>&1
+    copy="xclip -selection clipboard -i"
+    copy="xclip -selection clipboard -o"
 else
     copy="xsel -i -b"
     paste="xsel -o -b"
