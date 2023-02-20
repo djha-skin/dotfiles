c72359a9be92d3d036de79d13193e020cfc14e50
Daniel Jay Haskin <me@djha.skin>
2023-02-19 19:37:36 -0700

    Add sync gmail

--- Windows/Executables/sync-gmail.ps1
+++
@@ -1,8 +0,0 @@
-himalaya --disable-cache -o json -a gmail folders list `
-    | jq -r '.[].name' `
-    | where-object  { $_ -notin @(
-    "[Gmail]All Mail"
-    "[Gmail]Chats",
-    "[Gmail]Spam") } | foreach-object {
-    himalaya -a gmail -f "${_}" accounts sync
-}
