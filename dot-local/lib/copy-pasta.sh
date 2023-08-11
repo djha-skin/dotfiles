e67c7432c323c83c5d3e23dd7d7db6b7022b991e
Daniel Jay Haskin <me@djha.skin>
2023-08-11 14:56:19 -0600

    Copy pasta

--- dot-local/lib/copy-pasta.sh
+++ dot-local/lib/copy-pasta.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
-if which wl-copy >/dev/null 2>&1
-then
-    copy="wl-copy"
-    paste="wl-paste -n"
-elif which pbcopy >/dev/null 2>&1
+if which pbcopy >/dev/null 2>&1
 then
     copy="pbcopy"
     paste="pbpaste"
-elif which xclip >/dev/null 2>&1
+# https://unix.stackexchange.com/a/325972/9696
+elif [ "$(loginctl show-session $(loginctl | awk '/tty/ {print $1}') -p Type | awk -F= '{print $2}')" = "x11" ]
 then
-    copy="xclip -selection clipboard -i"
-    paste="xclip -selection clipboard -o"
+    if which xclip >/dev/null 2>&1
+    then
+        copy="xclip -selection clipboard -i"
+        paste="xclip -selection clipboard -o"
+    else
+        copy="xsel -i -b"
+        paste="xsel -o -b"
+    fi
 else
-    copy="xsel -i -b"
-    paste="xsel -o -b"
+    if which wl-copy >/dev/null 2>&1
+    then
+        copy="wl-copy"
+        paste="wl-paste -n"
+    fi
 fi
