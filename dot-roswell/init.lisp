7703829fcc98ef0f53f364b08f57aff62e75b1b0
Daniel Jay Haskin <me@djha.skin>
2024-03-31 18:49:46 -0600

    Done with roswell

--- dot-roswell/init.lisp
+++ dot-roswell/init.lisp
@@ -1,7 +1,25 @@
 (require '#:asdf)
 
+
 #+ecl
 (require '#:cmp)
+
+#+(and windows (not ocicl))
+(when (probe-file #P"C:/Users/bhw/AppData/Local/ocicl/ocicl-runtime.lisp")
+  (load #P"C:/Users/bhw/AppData/Local/ocicl/ocicl-runtime.lisp"))
+;; Any systems you install in C:/Users/bhw/AppData/Local/ocicl/
+;; will be available globally unless you comment out this line:
+#+windows
+(asdf:initialize-source-registry '(:source-registry :ignore-inherited-configuration (:tree #P"C:/Users/bhw/AppData/Local/ocicl/")))
+
+#-(or windows ocicl)
+(when (probe-file #P"/home/skin/.local/share/ocicl/ocicl-runtime.lisp")
+  (load #P"/home/skin/.local/share/ocicl/ocicl-runtime.lisp"))
+;; Any systems you install in C:/Users/bhw/AppData/Local/ocicl/
+;; will be available globally unless you comment out this line:
+#-windows
+(asdf:initialize-source-registry '(:source-registry :ignore-inherited-configuration (:tree #P"/home/skin/local/share/ocicl/")))
+
 
 ;;; The following lines added by ql:add-to-init-file:
 ;;#-quicklisp
@@ -10,13 +28,19 @@
 ;;  (when (probe-file quicklisp-init)
 ;;    (load quicklisp-init)))
 ;;
-(defun nvim (fname) 
-  (multiple-value-bind 
+
+(defun nvim (fname)
+  (multiple-value-bind
     (out err code)
     (uiop:run-program (list "nvim-qt" fname))
+
     (declare (ignore out)
              (ignore err))
     (eql code 0)))
+
+
+
+
 
 #+sbcl
 (push #'nvim *ed-functions*)
