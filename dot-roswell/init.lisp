1874046dd13f25b74a91c887652cc79eb89918ce
Daniel Jay Haskin <dan@djhaskin.com>
2024-04-12 13:09:56 -0600

    But better

--- dot-roswell/init.lisp
+++ dot-roswell/init.lisp
@@ -7,33 +7,36 @@
 #+(and windows (not ocicl))
 (when (probe-file #P"C:/Users/bhw/AppData/Local/ocicl/ocicl-runtime.lisp")
   (load #P"C:/Users/bhw/AppData/Local/ocicl/ocicl-runtime.lisp"))
-;; Any systems you install in C:/Users/bhw/AppData/Local/ocicl/
-;; will be available globally unless you comment out this line:
+;;; Any systems you install in C:/Users/bhw/AppData/Local/ocicl/
+;;; will be available globally unless you comment out this line:
 #+windows
 (asdf:initialize-source-registry '(:source-registry :ignore-inherited-configuration (:tree #P"C:/Users/bhw/AppData/Local/ocicl/")))
 
+
+;;; https://github.com/ocicl/ocicl.git
 #-(or windows ocicl)
 (when (probe-file #P"/home/skin/.local/share/ocicl/ocicl-runtime.lisp")
   (load #P"/home/skin/.local/share/ocicl/ocicl-runtime.lisp"))
-;; Any systems you install in C:/Users/bhw/AppData/Local/ocicl/
-;; will be available globally unless you comment out this line:
+;;; Any systems you install in C:/Users/bhw/AppData/Local/ocicl/
+;;; will be available globally unless you comment out this line:
 #-windows
 (asdf:initialize-source-registry '(:source-registry :ignore-inherited-configuration (:tree #P"/home/skin/local/share/ocicl/")))
 
 
 ;;; The following lines added by ql:add-to-init-file:
-;;#-quicklisp
-;;(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
-;;                                       (user-homedir-pathname))))
-;;  (when (probe-file quicklisp-init)
-;;    (load quicklisp-init)))
-;;
+;;;#-quicklisp
+;;;(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
+;;;                                       (user-homedir-pathname))))
+;;;  (when (probe-file quicklisp-init)
+;;;    (load quicklisp-init)))
+;;;
 
 (defun nvim (fname)
   (multiple-value-bind
     (out err code)
-    (uiop:run-program (list "nvim-qt" fname))
-
+    (uiop:run-program (list "nvim" fname)
+                      :output :interactive
+                      :input :interactive)
     (declare (ignore out)
              (ignore err))
     (eql code 0)))
@@ -56,3 +59,5 @@
   (export 'C:*USER-CC-FLAGS*)
   (in-package #:cl-user)
   )
+
+(pushnew (uiop:getcwd) asdf:*central-registry*)
