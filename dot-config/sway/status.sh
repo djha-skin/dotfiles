cd58c330eb0c741ac9a13d6ddd566f166324f89a
Daniel Jay Haskin <dhaskin@kpmanalytics.com>
2025-01-10 15:03:29 -0700

    Fix status

--- dot-config/sway/status.sh
+++ dot-config/sway/status.sh
@@ -6,7 +6,7 @@ battery_status=$(cat /sys/class/power_supply/BAT0/status)
 battery_power=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk -F ': *' '/percentage/{print $2}')
 
 # https://unix.stackexchange.com/a/230533
-volume=$(pactl list sinks | grep '^[[:space:]]Volume:' |     head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
+volume=$(pactl list sinks | grep '^[[:space:]]Volume:' |     head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')%
 brightness=$(brightnessctl | awk '-F[()]' '/Current brightness/{print $2}')
 
 # Emojis and characters for the status bar
