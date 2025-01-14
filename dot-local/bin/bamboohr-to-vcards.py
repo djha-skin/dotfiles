3f0c949031f5c50480356fb446c7ec2945c76b44
Daniel Jay Haskin <dhaskin@kpmanalytics.com>
2025-01-14 09:35:38 -0700

    Vcards

--- dot-local/bin/bamboohr-to-vcards.py
+++ dot-local/bin/bamboohr-to-vcards.py
@@ -1,20 +1,26 @@
+import copy
 import re
 import pprint
 import sys
 from pathlib import Path
+import uuid
+import os
 # Go to company directory in Bamboo HR
 # Select all, copy and paste to text file
 # Then run this
 # https://gist.github.com/gerwin3/ef9ae04d2b156c405db45dc70b93f716
+
 
 """
 This little script can generate a valid .vcf (vCard). It will ask you to fill
 in some details and write the vcf-file.
 """
 
+NAMESPACE = uuid.UUID('991268fe-d292-11ef-b006-879e80384164')
+
 def scrape(filename):
     find_name = re.compile(r'^(?P<first_name>[A-Z][A-Za-z\'-]*) (?P<last_name>.+)$')
-    find_reports = re.compile(r'^Reports to (?P<boss_first_name>[A-Z][A-Za-z\'-]*) (?P<boss_last_name>[A-Z][A-Za-z\'-]*)$')
+    find_reports = re.compile(r'^Reports to (?P<boss_first_name>[A-Z][A-Za-z\'-]*) (?P<boss_last_name>.+)$')
     find_email = re.compile(r'(?P<email>[^@]+@[^@]+)$')
     find_out = re.compile(r'^OUT (?P<out_info>.+)$')
     find_phone_number = re.compile(r'^(?P<phone_number>[0-9-() ]+)$')
@@ -22,18 +28,21 @@ def scrape(filename):
     find_new_section = re.compile(r'^[A-Z]$')
     find_direct_reports = re.compile(r'^[0-9]+ direct reports?$')
 
-    contacts = []
+    contacts = {}
     with open(filename, 'r', encoding='utf-8', errors='ignore') as f:
         line = f.readline()
         while line:
-            print(f"Line: {line}", file=sys.stderr)
             contact = {}
-            contacts.append(contact)
             names = find_name.search(line)
             if not names:
                 raise Exception('No name: ' + line)
-            contact['first_name'] = names.group('first_name').strip()
-            contact['last_name'] = names.group('last_name').strip()
+            first_name = names.group('first_name').strip()
+            last_name = names.group('last_name').strip()
+            contact['first_name'] = first_name
+            contact['last_name'] = last_name
+            name_uuid = uuid.uuid5(NAMESPACE, f'{first_name} {last_name}')
+            uuid_key = f'urn:uuid:{name_uuid}'
+            contacts[uuid_key] = contact
             line = f.readline()
             if line is None:
                 break
@@ -86,26 +95,38 @@ def scrape(filename):
                 line = f.readline()
                 if line is None:
                     break
-    return contacts
+    boss_contacts = copy.deepcopy(contacts)
+    for aurn, arecord in contacts.items():
+        if 'boss_first_name' in arecord:
+            for burn, brecord in contacts.items():
+                if arecord['boss_first_name'] == brecord['first_name'] and \
+                    arecord['boss_last_name'] == brecord['last_name']:
+                    boss_contacts[aurn]['boss_urn'] = burn
+    return boss_contacts
 
 def main():
     contacts = scrape('a')
-    for contact in contacts:
+
+
+    for curn, contact in contacts.items():
         first_name = contact.get('first_name')
         last_name = contact.get('last_name')
-        clean_last_name = re.sub('\W', '_', last_name)
+        clean_last_name = re.sub(r'\W', '_', last_name)
         company = contact.get('company')
         title = contact.get('title')
         phone_number = contact.get('phone_number')
         address = contact.get('address')
         email = contact.get('email')
-        vcard = make_vcard(first_name, last_name, company, title, phone_number, address, email)
+        boss_urn = contact.get('boss_urn')
+        vcard = make_vcard(curn, boss_urn, first_name, last_name, company, title, phone_number, address, email)
         sc = os.environ['SHORT_COMPANY_NAME']
         we = os.environ['WORK_EMAIL']
         vcf_file = f'/home/skin/Contacts/{sc}/{we}/{first_name.lower()}_{clean_last_name.lower()}.vcf'
         write_vcard(vcf_file, vcard)
 
 def make_vcard(
+        contact_urn,
+        boss_urn,
         first_name,
         last_name,
         company,
@@ -117,9 +138,12 @@ def make_vcard(
     card = [
         'BEGIN:VCARD',
         'VERSION:2.1',
+        f'UID:{contact_urn}'
         f'N:{last_name};{first_name}',
-        f'FN:{first_name} {last_name}'
+        f'FN:{first_name} {last_name}',
     ]
+    if boss_urn:
+        card.append('RELATED;TYPE=supervisor:{boss_urn}')
     if company:
         card.append(f'ORG:{company}')
     if title:
